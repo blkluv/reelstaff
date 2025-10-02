6ec651f07fbafe45b238e63cad7c666123b99433
Wizard of Hahz <info@blkluv.org>
2025-10-02 14:30:57 -0400

    Create ServiceStandards.tsx

---
+++ components/ServiceStandards.tsx
@@ -0,0 +1,11 @@
+import { Certification } from '@/types'
+import { Shield, Award, CheckCircle } from 'lucide-react'
+
+interface ServiceStandardsProps {
+  certifications: Certification[]
+}
+
+export default function ServiceStandards({ certifications }: ServiceStandardsProps) {
+  // Your ServiceStandards component code here
+  return <div>Service Standards Component</div>
+}
